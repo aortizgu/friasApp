6cdd9f90626f0aa1adae6d677d828e6f27bd701b
Adrián Ortiz Gutiérrez <aortiz@MacBook-Pro.local>
2020-05-30 18:56:29 +0200

    first commit

---
+++ include/exampleclass.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+class exampleclass
+{
+private:
+    /* data */
+public:
+    exampleclass(/* args */);
+    ~exampleclass();
+};
+
+exampleclass::exampleclass(/* args */)
+{
+}
+
+exampleclass::~exampleclass()
+{
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "exampleclass.hpp"
+
+int main(int, char**) {
+    exampleclass c;
+    std::cout << "Hello, world!\n";
+}
